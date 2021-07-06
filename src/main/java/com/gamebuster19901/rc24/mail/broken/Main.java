d4f3d9fe8ce9daefceebe962d24c6e3609e29da3
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-07-06 17:56:54 -0400

    it finally fucking works

--- src/main/java/com/gamebuster19901/rc24/mail/broken/Main.java
+++ src/main/java/com/gamebuster19901/rc24/mail/broken/Main.java
@@ -19,6 +19,7 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMultipart;
 import javax.mail.internet.PreencodedMimeBodyPart;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -34,6 +35,13 @@ public class Main {
 	public static final String SERVER = "rc24.xyz";
 	
 	public static void main(String[] args) throws IOException, MessagingException, InterruptedException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		for(int i = 0; i < 3; i++) {
+			send();
+		}
+		receive();
+	}
+	
+	public static void send() {
 		for(int i = 65; i < 66; i++) {
 			CloseableHttpClient client = HttpClients.createDefault();
 			File secretFile = new File("./mail.secret");
@@ -42,17 +50,88 @@ public class Main {
 			HttpPost request;
 			HttpPost dupe;
 			BufferedReader fileReader = null;
+			try {
+				fileReader = new BufferedReader(new FileReader(secretFile));
+				wiiID = fileReader.readLine();
+				password = fileReader.readLine();
+				request = new HttpPost("https://mtw." + SERVER + "/cgi-bin/send.cgi?mlid=" + wiiID + "&passwd=" + password + "&maxsize=11534336");
+				
+				String from = wiiID + "@rc24.xyz";
+				String to = "w0633303276188884@rc24.xyz";
+				
+				//request.addHeader("MAIL FROM", from);
+				//request.addHeader("RCPT TO", to);
+				//request.addHeader("From", from);
+				//request.addHeader("To", to);
+				//request.addHeader("Subject", "G");
+				request.addHeader("Content-Type", "multipart/form-data; boundary=t9Sf4yfjf1RtvDu3AA");
+				
+				String s = 					
+						"Content-Disposition: form-data; name=\"mlid\"\r\n"
+						+ "mlid="+wiiID+"\r\n"
+						+ "passwd="+password+"\r\n"
+						+ "--t9Sf4yfjf1RtvDu3AA\r\n"
+						+ "Content-Disposition: form-data; name=\"m1\"\r\n"
+						+ "\r\n"
+						+ "MAIL_FROM: " + from + "\r\n"
+						+ "RCPT TO: " + to + "\r\n"
+						+ "From: " + from + "\r\n"
+						+ "To: " + to + "\r\n"
+						+ "Subject: =?UTF-16BE?B?AFkAbwB1AHIAIABnAGEAbQBlAHMAIABhAHIAZQAgAHIAZQBhAGQAeQAh?=\r\n"
+						+ "X-Wii-AppId: 2-48414541-0001\r\n"
+						+ "X-Wii-Cmd: 00044001\r\n"
+						+ "Content-Type: text/plain; charset=utf-16be\r\n"
+						+ "Content-Transfer-Encoding: base64\r\n"
+						+ "\r\n"
+						+ "AHQAZQBzAHQ=\r\n"
+						+ "\r\n"
+						+ "--t9Sf4yfjf1RtvDu3AA--";
+				
+				StringEntity e = new StringEntity(s);
+				
+				MimeMultipart mp = new MimeMultipart(e.toString());
+				
+				request.setEntity(e);
+				
+				Header[] headers = request.getAllHeaders();
+				for(Header header : headers) {
+					System.err.println(header.getName() + ": " + header.getValue());
+				}
+				System.err.println(s);
+				
+				HttpResponse response = client.execute(request);
+				if(response != null) {
+					ByteArrayOutputStream logStream = new ByteArrayOutputStream();
+					response.getEntity().writeTo(logStream);
+					LOGGER.log(Level.INFO, logStream.toString());
+					logStream.close();
+				}
+				
+			}
+			catch(Throwable t) {
+				t.printStackTrace();
+			}
+		}
+	}
+	
+	public static void sendOld() throws IOException, MessagingException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, InterruptedException {
+		for(int i = 65; i < 66; i++) {
+			CloseableHttpClient client = HttpClients.createDefault();
+			File secretFile = new File("./mail.secret");
+			String wiiID;
+			String password;
+			HttpPost request;
+			BufferedReader fileReader = null;
 			InputStreamReader mailReader = null;
 			try {
 				fileReader = new BufferedReader(new FileReader(secretFile));
 				wiiID = fileReader.readLine();
 				password = fileReader.readLine();
 				request = new HttpPost("https://mtw." + SERVER + "/cgi-bin/send.cgi?mlid=" + wiiID + "&passwd=" + password + "&maxsize=11534336");
-				dupe = new HttpPost("https://mtw." + SERVER + "/cgi-bin/send.cgi?mlid=" + wiiID + "&passwd=" + password + "&maxsize=11534336");
 				
 				WiiMessage message = new WiiMessage(Session.getDefaultInstance(System.getProperties()));
 				
-				message.setEnvelopeFrom(wiiID + "@rc24.xyz");
+				message.addHeader("MAIL FROM", wiiID + "@rc24.xyz");
 				
 				MimeMultipart multiPart = new MimeMultipart();
 				ContentType contentType = new ContentType("multipart", "mixed", null);
@@ -62,16 +141,12 @@ public class Main {
 				field.set(multiPart, contentType.toString());
 				
 				EmptyBodyPart authPart = new EmptyBodyPart();
-				authPart.setDisposition("form-data; name=\"mlid\"");
-				authPart.addHeader("mlid", wiiID);
-				authPart.addHeader("passwd", password);
+				authPart.setDisposition("form-data; name=\"mlid\" mlid=" + wiiID + " passwd=" + password);
 				//authPart.setText(null);
 				password = null;
 				
 				MimeBodyPart messagePart = new PreencodedMimeBodyPart("base64");
 				messagePart.setDisposition("form-data; name=\"m1\"");
-				messagePart.addHeader("MAIL FROM", wiiID + "@rc24.xyz");
-				messagePart.addHeader("RCPT TO", wiiID + "@rc24.xyz");
 				messagePart.addHeader("From", wiiID + "@rc24.xyz");
 				messagePart.addHeader("To", wiiID + "@rc24.xyz");
 				messagePart.addHeader("Subject", "AEIAYQB6AGkAbgBnAGE=");
@@ -104,19 +179,13 @@ public class Main {
 				output = output.replace("\r\n\r\n--" + boundary, "\r\n--" + boundary);
 	
 				request.setEntity(new StringEntity(output));
-				dupe.setEntity(new StringEntity(output));
 				
-				HttpEntity responseEntity = dupe.getEntity();
-				if(responseEntity != null) {
-					ByteArrayOutputStream logStream = new ByteArrayOutputStream();
-					dupe.getEntity().writeTo(logStream);
-					LOGGER.log(Level.INFO, new String(logStream.toByteArray()));
-					logStream.close();
-				}
+				HttpEntity responseEntity = request.getEntity();
 				
 				HttpResponse response = client.execute(request);
 				if(response != null) {
 					ByteArrayOutputStream logStream = new ByteArrayOutputStream();
+					request.getEntity().writeTo(logStream);
 					response.getEntity().writeTo(logStream);
 					LOGGER.log(Level.INFO, new String(logStream.toByteArray()));
 					logStream.close();
@@ -143,8 +212,8 @@ public class Main {
 				}
 			}
 		}
-		System.err.println("Waiting 15 seconds to retrieve mail...");
-		Thread.sleep(15000);
+		System.err.println("Waiting 5 seconds to retrieve mail...");
+		Thread.sleep(5000);
 		receive();
 	}
 	
